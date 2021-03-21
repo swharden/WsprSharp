70641b2d37c1cf5f5efb659be7e161ca95d8e4ba
Scott W Harden <swharden@gmail.com>
2021-03-21 18:45:06 -0400

    refactor toward simpler methods

--- src/WsprSharp/OldEncoder.cs
+++ src/WsprSharp/OldEncoder.cs
@@ -9,123 +9,120 @@ using System.Linq;
 
 public class OldEncoder
 {
-    public const byte WSPR_SYMBOL_COUNT = 162;
+    /// <summary>
+    /// Number of bits in a WSPR transmission
+    /// </summary>
     public const byte WSPR_BIT_COUNT = 162;
-    public const byte WSPR_MESSAGE_SIZE = 11;
-
-    public byte[] symbols = new byte[162];
-
-    private readonly byte[] SyncVector = new byte[] {
-        1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0,
-        0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0,
-        0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0,
-        0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1,
-        0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0,
-        0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0 };
 
     /// <summary>
-    /// Encode a message into a 7-byte array
+    /// Total number of bytes in a WSPR transmission
+    /// </summary>
+    public const byte WSPR_MESSAGE_SIZE = 11;
+
+    /// <summary>
+    /// Convert station information into a 7-byte array 
+    /// using JTEncode's WSPR bit packing algorythm
     /// </summary>
     public byte[] GetMessageBytes(string callsign, string location, byte power)
     {
-        UInt32 intA;
-        UInt32 intB;
-
+        // pack callsign data into a 32-bit integer
         char[] callChars = callsign.ToCharArray();
+        uint intA;
+        intA = WsprCode(callChars[0]);
+        intA = intA * 36 + WsprCode(callChars[1]);
+        intA = intA * 10 + WsprCode(callChars[2]);
+        intA = (uint)(intA * 27 + (WsprCode(callChars[3]) - 10));
+        intA = (uint)(intA * 27 + (WsprCode(callChars[4]) - 10));
+        intA = (uint)(intA * 27 + (WsprCode(callChars[5]) - 10));
 
-        intA = GetCharacterCode(callChars[0]);
-
-        intA = intA * 36 + GetCharacterCode(callChars[1]);
-        intA = intA * 10 + GetCharacterCode(callChars[2]);
-        intA = (uint)(intA * 27 + (GetCharacterCode(callChars[3]) - 10));
-        intA = (uint)(intA * 27 + (GetCharacterCode(callChars[4]) - 10));
-        intA = (uint)(intA * 27 + (GetCharacterCode(callChars[5]) - 10));
-
+        // pack location and power into a 32-bit integer
         char[] locChars = location.ToCharArray();
+        uint intB;
         intB = (uint)((179 - 10 * (locChars[0] - 'A') - (locChars[2] - '0')) * 180 +
-                          10 * (locChars[1] - 'A') + (locChars[3] - '0'));
+                             10 * (locChars[1] - 'A') + (locChars[3] - '0'));
         intB = (intB * 128) + power + 64;
 
-        byte[] messageBytes = new byte[7];
-        messageBytes[3] = ((byte)((intA & 0xF) << 4));
+        // translate the two integers into a 7-byte array
+        byte[] bytes = new byte[7];
+        bytes[3] = ((byte)((intA & 0xF) << 4));
         intA >>= 4;
-        messageBytes[2] = ((byte)(intA & 0xFF));
+        bytes[2] = ((byte)(intA & 0xFF));
         intA >>= 8;
-        messageBytes[1] = ((byte)(intA & 0xFF));
+        bytes[1] = ((byte)(intA & 0xFF));
         intA >>= 8;
-        messageBytes[0] = ((byte)(intA & 0xFF));
-        messageBytes[6] = ((byte)((intB & 0x3) << 6));
+        bytes[0] = ((byte)(intA & 0xFF));
+        bytes[6] = ((byte)((intB & 0x3) << 6));
         intB >>= 2;
-        messageBytes[5] = ((byte)(intB & 0xFF));
+        bytes[5] = ((byte)(intB & 0xFF));
         intB >>= 8;
-        messageBytes[4] = ((byte)(intB & 0xFF));
+        bytes[4] = ((byte)(intB & 0xFF));
         intB >>= 8;
-        messageBytes[3] = (byte)(messageBytes[3] | (intB & 0xF));
-        return messageBytes;
+        bytes[3] = (byte)(bytes[3] | (intB & 0xF));
+        return bytes;
     }
 
-    public byte[] ConvolveMessage(byte[] messageBytes)
+    /// <summary>
+    /// Convolve a byte array using JTEncode's convolution method
+    /// </summary>
+    public byte[] Convolve(byte[] input, int bitSize = WSPR_BIT_COUNT)
     {
-        // the input message is only 7 bytes but convolution expects 11 bytes
-        byte[] c = new byte[WSPR_MESSAGE_SIZE];
-        Array.Copy(messageBytes, 0, c, 0, messageBytes.Length);
+        byte[] paddedInput = new byte[WSPR_MESSAGE_SIZE];
+        Array.Copy(input, 0, paddedInput, 0, input.Length);
 
-        byte[] s = new byte[162];
-        UInt32 reg_0 = 0;
-        UInt32 reg_1 = 0;
-        byte input_bit;
-        byte parity_bit;
-        byte bit_count = 0;
+        byte[] output = new byte[bitSize];
+        UInt32 reg0 = 0;
+        UInt32 reg1 = 0;
+        byte inputBit;
+        byte parityBit;
+        byte bitIndex = 0;
         byte i, j, k;
-        byte bit_size = 162;
 
         for (i = 0; i < WSPR_MESSAGE_SIZE; i++)
         {
             for (j = 0; j < 8; j++)
             {
                 // Set input bit according the MSB of current element
-                input_bit = (byte)((((c[i] << j) & 0x80) == 0x80) ? 1 : 0);
+                inputBit = (byte)((((paddedInput[i] << j) & 0x80) == 0x80) ? 1 : 0);
 
                 // Shift both registers and put in the new input bit
-                reg_0 <<= 1;
-                reg_1 <<= 1;
-                reg_0 |= input_bit;
-                reg_1 |= input_bit;
+                reg0 <<= 1;
+                reg1 <<= 1;
+                reg0 |= inputBit;
+                reg1 |= inputBit;
 
                 // AND Register 0 with feedback taps, calculate parity
-                UInt32 reg_temp = reg_0 & 0xf2d05351;
-                parity_bit = 0;
+                UInt32 regTemp = reg0 & 0xf2d05351;
+                parityBit = 0;
                 for (k = 0; k < 32; k++)
                 {
-                    parity_bit = (byte)(parity_bit ^ (reg_temp & 0x01));
-                    reg_temp >>= 1;
+                    parityBit = (byte)(parityBit ^ (regTemp & 0x01));
+                    regTemp >>= 1;
                 }
-                s[bit_count] = parity_bit;
-                bit_count++;
+                output[bitIndex] = parityBit;
+                bitIndex++;
 
                 // AND Register 1 with feedback taps, calculate parity
-                reg_temp = reg_1 & 0xe4613c47;
-                parity_bit = 0;
+                regTemp = reg1 & 0xe4613c47;
+                parityBit = 0;
                 for (k = 0; k < 32; k++)
                 {
-                    parity_bit = (byte)(parity_bit ^ (reg_temp & 0x01));
-                    reg_temp >>= 1;
+                    parityBit = (byte)(parityBit ^ (regTemp & 0x01));
+                    regTemp >>= 1;
                 }
 
-                s[bit_count] = parity_bit;
-                bit_count++;
-                if (bit_count >= bit_size)
-                {
+                output[bitIndex] = parityBit;
+                bitIndex++;
+                if (bitIndex >= bitSize)
                     break;
-                }
             }
         }
-        return s;
+
+        return output;
     }
 
     public byte[] Interleave(byte[] s)
     {
-        byte[] d = new byte[162];
+        byte[] d = new byte[WSPR_BIT_COUNT];
 
         byte rev, index_temp, i, j, k;
 
@@ -161,9 +158,12 @@ public class OldEncoder
         return d;
     }
 
-    public byte[] MergeSyncVector(byte[] bytes)
+    /// <summary>
+    /// Combine data with a standard synchronization array that has good auto-correlation properties.
+    /// </summary>
+    public byte[] IntegrateSyncValues(byte[] data)
     {
-        byte[] standardSyncBytes =
+        byte[] sync =
         {
              1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0,
              1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0,
@@ -175,42 +175,28 @@ public class OldEncoder
              1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0
         };
 
-        byte[] merged = new byte[162];
-        for (int i = 0; i < 162; i++)
-            merged[i] = (byte)(standardSyncBytes[i] + (2 * bytes[i]));
+        if (data.Length != sync.Length)
+            throw new ArgumentException($"Input data must be the same size as the sync array ({sync.Length} elements)");
 
-        return merged;
+        return Enumerable
+            .Range(0, WSPR_BIT_COUNT)
+            .Select(i => data[i] * 2 + sync[i])
+            .Select(x => (byte)x)
+            .ToArray();
     }
 
-    public byte GetCharacterCode(char c)
+    /// <summary>
+    /// Encode a character as a number according to JTEncode's standard
+    /// </summary>
+    public byte WsprCode(char c)
     {
-        bool isdigit;
-        byte result = 255;
-
-        isdigit = false;
-
-        if (c >= '0')
-        {
-            if (c <= '9')
-                isdigit = true;
-        }
-
-        if (isdigit == true)
-        {
-            result = (byte)(c - 48);
-        }
+        if (char.IsDigit(c))
+            return (byte)(c - 48);
+        else if (c == ' ')
+            return 36;
+        else if (c >= 'A' && c <= 'Z')
+            return (byte)(c - 55);
         else
-        {
-            if (c == ' ')
-                result = 36;
-
-            if (c >= 'A')
-            {
-                if (c <= 'Z')
-                    result = (byte)(c - 55);
-            }
-        }
-
-        return result;
+            throw new InvalidOperationException($"character {c} is not allowed");
     }
 }
--- tests/EncoderTests.cs
+++ tests/EncoderTests.cs
@@ -7,53 +7,14 @@ namespace WsprSharp.Tests
 {
     public class EncoderTests
     {
-        /// <summary>
-        /// Return a byte array as a hex string
-        /// </summary>
-        private string GetHexString(byte[] bytes, string separator = " ") =>
-            string.Join(separator, bytes.Select(x => x.ToString("x2")));
-
-        /// <summary>
-        /// return a CSV-formatted block of text given a byte array
-        /// </summary>
-        private string GetSymbolString(byte[] bytes, string separator = ",", int valuesPerLine = 30)
-        {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < bytes.Length; i++)
-            {
-                sb.Append(bytes[i]);
-                sb.Append(separator);
-                if (i % valuesPerLine == valuesPerLine - 1)
-                    sb.Append("\n");
-            }
-            return sb.ToString();
-        }
-
         [Test]
         public void Test_OldEcoder_KnownValues()
         {
             var encoder = new OldEncoder();
-
-            // translate callsign, location, and power into a message
             byte[] message = encoder.GetMessageBytes(" W8ZLW", "AA00", 3);
-            string knownMessageHex = "f9 72 f2 8f bb 90 c0";
-            Assert.AreEqual(knownMessageHex, GetHexString(message));
-
-            // process the message to get data
-            byte[] convolvedMessage = encoder.ConvolveMessage(message);
+            byte[] convolvedMessage = encoder.Convolve(message);
             byte[] dataSymbols = encoder.Interleave(convolvedMessage);
-            byte[] knownDataSymbols = new byte[] {
-                1,0,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,1,1,1,0,0,1,1,
-                1,0,0,0,1,1,1,0,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,0,0,0,1,0,1,1,
-                0,0,0,1,1,0,1,0,1,1,0,0,0,0,0,1,0,0,0,0,0,1,1,1,0,0,1,0,0,1,
-                1,1,0,1,1,0,1,0,1,1,1,1,1,0,0,1,1,1,0,1,1,0,1,0,1,0,1,0,0,1,
-                0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,1,1,1,0,0,1,
-                1,0,0,0,1,0,1,1,1,0,1,0
-            };
-            Assert.AreEqual(knownDataSymbols, dataSymbols);
-
-            // translate the data into channels
-            byte[] channelSymbols = encoder.MergeSyncVector(dataSymbols);
+            byte[] channelSymbols = encoder.IntegrateSyncValues(dataSymbols);
             byte[] knownChannelSymbols = new byte[]
             {
                 3,1,2,2,0,0,0,0,1,0,0,0,3,3,3,2,2,2,1,0,0,1,0,3,3,3,1,0,2,2,
