8727a571cb9ff0f9a6dbb83c0cd6363ca9eaaf51
Scott W Harden <swharden@gmail.com>
2021-03-21 21:30:43 -0400

    WSPR Inspector

---
+++ src/WsprInspector/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿
+namespace WsprInspector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbCallsign = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tbLocation = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.rtbLevels = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cbPower = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbCallsign);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(92, 47);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Callsign";
+            // 
+            // tbCallsign
+            // 
+            this.tbCallsign.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbCallsign.Location = new System.Drawing.Point(6, 19);
+            this.tbCallsign.Name = "tbCallsign";
+            this.tbCallsign.Size = new System.Drawing.Size(80, 20);
+            this.tbCallsign.TabIndex = 0;
+            this.tbCallsign.Text = "AJ4VD";
+            this.tbCallsign.TextChanged += new System.EventHandler(this.tbCallsign_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbLocation);
+            this.groupBox2.Location = new System.Drawing.Point(110, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(83, 47);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Location";
+            // 
+            // tbLocation
+            // 
+            this.tbLocation.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbLocation.Location = new System.Drawing.Point(6, 19);
+            this.tbLocation.Name = "tbLocation";
+            this.tbLocation.Size = new System.Drawing.Size(71, 20);
+            this.tbLocation.TabIndex = 0;
+            this.tbLocation.Text = "EL89";
+            this.tbLocation.TextChanged += new System.EventHandler(this.tbLocation_TextChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.cbPower);
+            this.groupBox3.Location = new System.Drawing.Point(199, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(85, 47);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Power (dB)";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.tbMessage);
+            this.groupBox4.Location = new System.Drawing.Point(290, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(174, 47);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Message (hex)";
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbMessage.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbMessage.Location = new System.Drawing.Point(6, 19);
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.ReadOnly = true;
+            this.tbMessage.Size = new System.Drawing.Size(162, 20);
+            this.tbMessage.TabIndex = 0;
+            this.tbMessage.Text = "F9 72 F2 8F BB 90 C0 ";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox5.Controls.Add(this.rtbLevels);
+            this.groupBox5.Location = new System.Drawing.Point(12, 65);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(452, 111);
+            this.groupBox5.TabIndex = 3;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Transmission Packet";
+            // 
+            // rtbLevels
+            // 
+            this.rtbLevels.BackColor = System.Drawing.SystemColors.Control;
+            this.rtbLevels.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbLevels.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbLevels.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbLevels.Location = new System.Drawing.Point(3, 16);
+            this.rtbLevels.Name = "rtbLevels";
+            this.rtbLevels.ReadOnly = true;
+            this.rtbLevels.Size = new System.Drawing.Size(446, 92);
+            this.rtbLevels.TabIndex = 0;
+            this.rtbLevels.Text = resources.GetString("rtbLevels.Text");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Navy;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 179);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 180);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cbPower
+            // 
+            this.cbPower.FormattingEnabled = true;
+            this.cbPower.Items.AddRange(new object[] {
+            "0",
+            "3",
+            "7",
+            "10",
+            "13",
+            "17",
+            "20",
+            "23",
+            "27",
+            "30",
+            "33",
+            "37",
+            "40",
+            "43",
+            "47",
+            "50",
+            "53",
+            "57",
+            "60"});
+            this.cbPower.Location = new System.Drawing.Point(6, 19);
+            this.cbPower.Name = "cbPower";
+            this.cbPower.Size = new System.Drawing.Size(73, 21);
+            this.cbPower.TabIndex = 5;
+            this.cbPower.Text = "3";
+            this.cbPower.SelectedIndexChanged += new System.EventHandler(this.cbPower_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 371);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "WSPR Inspector";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbCallsign;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tbLocation;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.RichTextBox rtbLevels;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox cbPower;
+    }
+}
+
---
+++ src/WsprInspector/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WsprInspector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Recalculate();
+        }
+
+        private void Recalculate()
+        {
+            var wspr = new WsprSharp.WsprTransmission(tbCallsign.Text, tbLocation.Text, double.Parse(cbPower.Text));
+
+            if (wspr.IsValid == false)
+            {
+                tbMessage.Text = "invalid input";
+                rtbLevels.Text = "invalid input";
+                return;
+            }
+
+            tbMessage.Text = wspr.GetMessageString();
+            rtbLevels.Text = wspr.GetLevelsString();
+            pictureBox1.Image = WsprSharp.WsprImage.MakeSpectrogram(wspr.Levels);
+        }
+
+        private void tbCallsign_TextChanged(object sender, EventArgs e) => Recalculate();
+        private void tbLocation_TextChanged(object sender, EventArgs e) => Recalculate();
+        private void cbPower_SelectedIndexChanged(object sender, EventArgs e) => Recalculate();
+    }
+}
