2f1be45811f5c334c726e97b3d05f19a8e5ae6e7
Scott W Harden <swharden@gmail.com>
2021-03-21 22:25:31 -0400

    improve layout

--- src/WsprInspector/Form1.Designer.cs
+++ src/WsprInspector/Form1.Designer.cs
@@ -35,12 +35,12 @@ namespace WsprInspector
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.tbLocation = new System.Windows.Forms.TextBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.cbPower = new System.Windows.Forms.ComboBox();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.tbMessage = new System.Windows.Forms.TextBox();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
             this.rtbLevels = new System.Windows.Forms.RichTextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.cbPower = new System.Windows.Forms.ComboBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -52,9 +52,11 @@ namespace WsprInspector
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.tbCallsign);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(14, 14);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(92, 47);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Size = new System.Drawing.Size(107, 54);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Callsign";
@@ -63,9 +65,10 @@ namespace WsprInspector
             // 
             this.tbCallsign.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbCallsign.Location = new System.Drawing.Point(6, 19);
+            this.tbCallsign.Location = new System.Drawing.Point(7, 22);
+            this.tbCallsign.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.tbCallsign.Name = "tbCallsign";
-            this.tbCallsign.Size = new System.Drawing.Size(80, 20);
+            this.tbCallsign.Size = new System.Drawing.Size(93, 23);
             this.tbCallsign.TabIndex = 0;
             this.tbCallsign.Text = "AJ4VD";
             this.tbCallsign.TextChanged += new System.EventHandler(this.tbCallsign_TextChanged);
@@ -73,9 +76,11 @@ namespace WsprInspector
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.tbLocation);
-            this.groupBox2.Location = new System.Drawing.Point(110, 12);
+            this.groupBox2.Location = new System.Drawing.Point(128, 14);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(83, 47);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox2.Size = new System.Drawing.Size(97, 54);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Location";
@@ -84,9 +89,10 @@ namespace WsprInspector
             // 
             this.tbLocation.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbLocation.Location = new System.Drawing.Point(6, 19);
+            this.tbLocation.Location = new System.Drawing.Point(7, 22);
+            this.tbLocation.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.tbLocation.Name = "tbLocation";
-            this.tbLocation.Size = new System.Drawing.Size(71, 20);
+            this.tbLocation.Size = new System.Drawing.Size(82, 23);
             this.tbLocation.TabIndex = 0;
             this.tbLocation.Text = "EL89";
             this.tbLocation.TextChanged += new System.EventHandler(this.tbLocation_TextChanged);
@@ -94,72 +100,14 @@ namespace WsprInspector
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.cbPower);
-            this.groupBox3.Location = new System.Drawing.Point(199, 12);
+            this.groupBox3.Location = new System.Drawing.Point(232, 14);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(85, 47);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox3.Size = new System.Drawing.Size(99, 54);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Power (dB)";
-            // 
-            // groupBox4
-            // 
-            this.groupBox4.Controls.Add(this.tbMessage);
-            this.groupBox4.Location = new System.Drawing.Point(290, 12);
-            this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(174, 47);
-            this.groupBox4.TabIndex = 1;
-            this.groupBox4.TabStop = false;
-            this.groupBox4.Text = "Message (hex)";
-            // 
-            // tbMessage
-            // 
-            this.tbMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbMessage.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbMessage.Location = new System.Drawing.Point(6, 19);
-            this.tbMessage.Name = "tbMessage";
-            this.tbMessage.ReadOnly = true;
-            this.tbMessage.Size = new System.Drawing.Size(162, 20);
-            this.tbMessage.TabIndex = 0;
-            this.tbMessage.Text = "F9 72 F2 8F BB 90 C0 ";
-            // 
-            // groupBox5
-            // 
-            this.groupBox5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox5.Controls.Add(this.rtbLevels);
-            this.groupBox5.Location = new System.Drawing.Point(12, 65);
-            this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(452, 111);
-            this.groupBox5.TabIndex = 3;
-            this.groupBox5.TabStop = false;
-            this.groupBox5.Text = "Transmission Packet";
-            // 
-            // rtbLevels
-            // 
-            this.rtbLevels.BackColor = System.Drawing.SystemColors.Control;
-            this.rtbLevels.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.rtbLevels.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rtbLevels.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rtbLevels.Location = new System.Drawing.Point(3, 16);
-            this.rtbLevels.Name = "rtbLevels";
-            this.rtbLevels.ReadOnly = true;
-            this.rtbLevels.Size = new System.Drawing.Size(446, 92);
-            this.rtbLevels.TabIndex = 0;
-            this.rtbLevels.Text = resources.GetString("rtbLevels.Text");
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.BackColor = System.Drawing.Color.Navy;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 179);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(776, 180);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
             // 
             // cbPower
             // 
@@ -184,24 +132,91 @@ namespace WsprInspector
             "53",
             "57",
             "60"});
-            this.cbPower.Location = new System.Drawing.Point(6, 19);
+            this.cbPower.Location = new System.Drawing.Point(7, 22);
+            this.cbPower.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.cbPower.Name = "cbPower";
-            this.cbPower.Size = new System.Drawing.Size(73, 21);
+            this.cbPower.Size = new System.Drawing.Size(84, 23);
             this.cbPower.TabIndex = 5;
             this.cbPower.Text = "3";
             this.cbPower.SelectedIndexChanged += new System.EventHandler(this.cbPower_SelectedIndexChanged);
             // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.tbMessage);
+            this.groupBox4.Location = new System.Drawing.Point(338, 14);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox4.Size = new System.Drawing.Size(203, 54);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Message (hex)";
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbMessage.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbMessage.Location = new System.Drawing.Point(7, 22);
+            this.tbMessage.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.ReadOnly = true;
+            this.tbMessage.Size = new System.Drawing.Size(188, 20);
+            this.tbMessage.TabIndex = 0;
+            this.tbMessage.Text = "F9 72 F2 8F BB 90 C0 ";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.rtbLevels);
+            this.groupBox5.Location = new System.Drawing.Point(14, 75);
+            this.groupBox5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox5.Size = new System.Drawing.Size(527, 128);
+            this.groupBox5.TabIndex = 3;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Transmission Packet";
+            // 
+            // rtbLevels
+            // 
+            this.rtbLevels.BackColor = System.Drawing.SystemColors.Control;
+            this.rtbLevels.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbLevels.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbLevels.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rtbLevels.Location = new System.Drawing.Point(4, 19);
+            this.rtbLevels.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.rtbLevels.Name = "rtbLevels";
+            this.rtbLevels.ReadOnly = true;
+            this.rtbLevels.Size = new System.Drawing.Size(519, 106);
+            this.rtbLevels.TabIndex = 0;
+            this.rtbLevels.Text = resources.GetString("rtbLevels.Text");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Navy;
+            this.pictureBox1.Location = new System.Drawing.Point(14, 207);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(905, 208);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 371);
+            this.ClientSize = new System.Drawing.Size(933, 428);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.groupBox5);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "Form1";
             this.Text = "WSPR Inspector";
             this.groupBox1.ResumeLayout(false);
