181fb5428b702c9c0b27486941929c5743dc8747
Scott W Harden <swharden@gmail.com>
2023-10-16 18:53:48 -0400

    tests: modernize syntax

---
+++ src/WsprSharpTests/QuickstartTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace WsprSharp.Tests;
+internal class QuickstartTests
+{
+    [Test]
+    public void Test_Quickstart_Demo()
+    {
+        WsprTransmission wspr = new("AJ4VD", "EL89", 3);
+
+        Console.WriteLine("Encoded Bytes:");
+        Console.WriteLine(string.Join(" ", wspr.Message));
+
+        Console.WriteLine("Transmission Frequency:");
+        Console.WriteLine(string.Join(" ", wspr.Levels));
+    }
+}
